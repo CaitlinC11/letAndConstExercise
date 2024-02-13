dd36f1dcff1de7fdd4c2f7a780e4f60eb630d854
CaitlinC11 <chr1111@live.com>
2024-02-13 17:58:23 -0500

    answers to questions

---
+++ letAndConst.js
@@ -0,0 +1,8 @@
+
+const PI = 3.14; // ES5 to ES2015. Remove the var and replaced with const
+
+// Quiz:
+// - What is the difference between ***var*** and ***let***? You cannot redeclare let but you can reassign and declare var
+// - What is the difference between **var** and ***const***? You cannot redeclare or reassign const but you can do both with var.
+// - What is the difference between **let** and **const**?  You can not redeclare or reassign using const but you can reassign with let.
+// - What is hoisting? variables are 'hoisted' to the top of the scope they are being delcared in. Do this before they are defined.
